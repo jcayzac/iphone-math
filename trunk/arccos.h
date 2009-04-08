6faff16f0959980cc87e9c2437db47d18c82c738
julien.cayzac <julien.cayzac@1ead45b2-2320-11de-99f3-63f2d503d578>
2009-04-08 14:38:23 +0000

    arccos() is really faster now

--- trunk/arccos.h
+++ trunk/arccos.h
@@ -5,17 +5,23 @@
 #include <math.h>
 
 // iPhone timings:
-//     fast_acos(): 157.5μs
-//     acosf(): 435.12μs
-// fast_acos() is 2.76268 times faster than acosf()
+//     fast_acos(): 0.06ms
+//     acosf():     0.44ms
+// fast_acos() is 7.2 times faster than acosf()
 // 
-// Maximum error: 0.035195 radians
-// Average error: 0.010294 radians
+// Maximum error: 0.06 radians
+// Average error: 0.01 radians
 static inline float arccos_radians(float x) {
-	float u=(fabsf(x)<.5f)
-		?(1.57079632679489661923-1.05199081698724154807*fabsf(x))
-		:2.f*sqrtf(1.f-sqrtf(fabsf(x)));
-	return (x<.0f)?3.14159265358979323846f-u:u;
+	union { float f; unsigned int b; } z;
+	z.f=x;
+	bool cond1 = (z.b&0x80000000); // negative?
+	z.b&=0x7fffffff; // fabsf
+	float s=sqrtf(z.f); // pipeline
+	float u=1.57079632679489661923f-1.05199081698724154807f*z.f;
+	bool cond2 = (z.b<0x3f400000); // <0.75?
+	
+	u=cond2?u:2.f*sqrtf(1.f-s);
+	return cond1?3.14159265358979323846f-u:u;
 }
 
 static inline float arccos_degrees(float x) {
